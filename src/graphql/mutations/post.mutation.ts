2abf3791d0d629c6163616843b632be9b5f2ed00
Vlad <vlad.nifos@gmail.com>
2020-09-19 12:39:23 +0300

    fix: Schema fixes

--- src/graphql/mutations/post.mutation.ts
+++ src/graphql/mutations/post.mutation.ts
@@ -7,31 +7,37 @@ export const postMutation = {
     }
   `,
   UPDATE_POST_TITLE: gql`
-    mutation UpdatePostTitle($title: String) {
-      UpdatePostTitle(title: $title)
+    mutation UpdatePostTitle($title: String, $id: String) {
+      UpdatePostTitle(title: $title, id: $id)
     }
   `,
   ADD_POST_DATA: gql`
-    mutation AddPostData($id: String, $type: Boolean, $title: String) {
-      AddPostData(id: $id, type: $type, type: $title)
+    mutation AddPostData($id: String, $pros: Boolean, $title: String) {
+      AddPostData(id: $id, pros: $pros, title: $title)
     }
   `,
   UPDATE_POST_DATA: gql`
     mutation UpdatePostData(
       $postId: String
       $dataTitle: String
+      $pros: Boolean
       $newDataTitle: String
     ) {
       UpdatePostData(
         postId: $postId
         dataTitle: $dataTitle
+        pros: $pros
         newDataTitle: $newDataTitle
       )
     }
   `,
   REMOVE_POST_DATA: gql`
-    mutation RemovePostData($postId: String, $dataTitle: String) {
-      RemovePostData(postId: $postId, dataTitle: $dataTitle)
+    mutation RemovePostData(
+      $postId: String
+      $dataTitle: String
+      $pros: Boolean
+    ) {
+      RemovePostData(postId: $postId, dataTitle: $dataTitle, pros: $pros)
     }
   `,
   DELETE_POST: gql`
